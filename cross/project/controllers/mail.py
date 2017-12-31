ec53d1b0ad70da041a306feecdc01de2529c40ed
Sitala <striboga@yandex.ru>
2017-12-31 17:14:04 +0200

    first

--- cross/project/controllers/mail.py
+++ cross/project/controllers/mail.py
@@ -6,15 +6,15 @@ from gmail import Message
 
 
 def send_mail(base_mail, to_mail, user_name, user_text):
-    try:
-        b = "".join(["Thanks 2018! <", base_mail, ">"])
+    # try:
+    b = "".join(["Thanks 2018! <", base_mail, ">"])
 
-        gm = gmail.GMail(b, 'sonic1980')
-        msg = Message('Спасибо от', to="me <{0}>".format(to_mail),
-                      text="Спасибо от {0}, сообщение: {1}".format(user_name, user_text))
-        gm.send(msg)
-    except Exception as ex:
-        pass
+    gm = gmail.GMail(b, 'sonic1980')
+    msg = Message('Спасибо от', to="me <{0}>".format(to_mail),
+                  text="Спасибо от {0}, сообщение: {1}".format(user_name, user_text))
+    gm.send(msg)
+    # except Exception as ex:
+    #     pass
 
 # def send_mail_key(base_mail, base_mailpass, to_mail, akey):
 #
